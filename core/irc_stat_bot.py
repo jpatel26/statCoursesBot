4cc5a0c635ea115692fca530f3da7818d35fac7f
Evan Zhang <evanlzhang@gmail.com>
2019-06-10 20:37:13 -0700

    fix 77

--- core/irc_stat_bot.py
+++ core/irc_stat_bot.py
@@ -96,7 +96,7 @@ class IrcStacia(irc.bot.SingleServerIRCBot):
         elif cmd == "usage":
             c.privmsg(self.channel, "Ask me a question about Statistics courses at CalPoly.")
         else:
-            c.privmsg(self.channel, self.response_fn(cmd).content[:512])
+            c.privmsg(self.channel, self.response_fn(cmd).content[:500])
 
 
 def bot_init(response_fn=lambda s: s, greeting_fn=lambda s: s):
